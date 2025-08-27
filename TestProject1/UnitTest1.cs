dba67e2b8de5ddb50277bfe7b650dddf7c9262b5
Oliver Mouaici <oliver.mouaici@outlook.com>
2025-08-27 10:42:44 +0200

    Update

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,15 +1,19 @@
 
-namespace TestProject
-{
-    namespace Calculator.Tests
+using Console_based_Calculator_1;
+
+
+    namespace TestProject1 
+
     {
         public class CalculatorTests
         {
-            private readonly CalculatorTests _calc;
+            private readonly Calculator _calc;
+
+
 
             public CalculatorTests()
             {
-                _calc = new CalculatorTests();
+                 _calc = new Calculator();
             }
 
             [Fact]
@@ -19,33 +23,32 @@ namespace TestProject
                 Assert.Equal(8, result);
             }
 
-            private int Add(int v1, int v2)
-            {
-                throw new NotImplementedException();
-            }
+            //private int Add(int v1, int v2)
+            //{
+            //    throw new NotImplementedException();
+            //}
 
-            [Fact]
-            public void SubtractShouldReturnCorrectDifference()
-            {
-                int result = _calc.Subtract_ShouldReturnCorrectDifference(10, 4);
-                Assert.Equal(6, result);
-            }
+            //[Fact]
+            //public void SubtractShouldReturnCorrectDifference()
+            //{
+            //    int result = _calc.Subtract_ShouldReturnCorrectDifference(10, 4);
+            //    Assert.Equal(6, result);
+            //}
 
-            private int Subtract_ShouldReturnCorrectDifference(int v1, int v2)
-            {
-                throw new NotImplementedException();
-            }
+            //private int Subtract_ShouldReturnCorrectDifference(int v1, int v2)
+            //{
+            //    throw new NotImplementedException();
+            //}
 
-            [Fact]
-            public void DivideByZeroShouldThrowException()
-            {
-                Assert.Throws<DivideByZeroException>(() => _calc.Divide_ByZero_ShouldThrowException(10, 0));
-            }
+            //[Fact]
+            //public void DivideByZeroShouldThrowException()
+            //{
+            //    Assert.Throws<DivideByZeroException>(() => _calc.Divide_ByZero_ShouldThrowException(10, 0));
+            //}
 
-            private void Divide_ByZero_ShouldThrowException(int v1, int v2)
-            {
-                throw new NotImplementedException();
-            }
+            //private void Divide_ByZero_ShouldThrowException(int v1, int v2)
+            //{
+            //    throw new NotImplementedException();
+            //}
         }
     }
-}
